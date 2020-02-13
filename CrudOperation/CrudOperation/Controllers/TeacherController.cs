b34775de35f8e333f4416875ff29a7f41f520be2
khalid javaid <khalid.javaid@nxb.com.pk>
2020-02-13 11:46:48 +0500

    Added New teacher controller

---
+++ CrudOperation/CrudOperation/Controllers/TeacherController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudOperation.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CrudOperation.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private SchoolContext schoolContext;
+        public TeacherController(ILogger<HomeController> logger, SchoolContext sc)
+        {
+            _logger = logger;
+            schoolContext = sc;
+        }
+        public IActionResult Index()
+        {
+            return View(schoolContext.Teacher);
+        }
+    }
+}
